02cceb39b4b3e0660713ae8eb7e11159edd81f55
marleneanton <the.marlene.anton@gmail.com>
2024-08-21 15:00:34 +0200

    Implements main and bonus methods as well as tests for each

---
+++ src/main/java/PasswordChecker.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class PasswordChecker {
+    public static void main(String[] args) {
+        System.out.println("Checked if 'hello' is valid: " + isValid("hello"));
+        System.out.println("Checked if '12356789' is valid: " + isValid("12356789"));
+        System.out.println("Checked if 'Password' is valid: " + isValid("Password"));
+        System.out.println("Checked if 'dajsio12fAFSfsaf35' is valid: " + isValid("dajsio12fAFSfsaf35"));
+    }
+
+    // overall pw checker function, calls all util functions
+    public static boolean isValid(String pw) {
+        return hasMinLength(pw) && containsDigits(pw)
+                && hasUpperAndLower(pw) && isNotCommonPW(pw);
+    }
+
+    // checks if pw has minimum length
+    public static boolean hasMinLength(String pw) {
+        return pw.length() >= 8;
+    }
+
+    // checks if pw contains at least 1 digit
+    public static boolean containsDigits(String pw) {
+        for (char c : pw.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // checks if pw has at least one upper and one lower case character
+    public static boolean hasUpperAndLower(String pw) {
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        for (char c : pw.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            }
+            if (Character.isLowerCase(c)) {
+                hasLower = true;
+            }
+            if (hasLower && hasUpper) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // checks if pw is not one from a list of common weak pws
+    public static boolean isNotCommonPW(String pw) {
+        String[] weakPWs = {"Password1", "Password123", "Password", "Aa345678"};
+        return !Arrays.asList(weakPWs).contains(pw);
+    }
+
+    // checks if pw has at least 1 special character
+    public static boolean hasSpecialChars(String pw) {
+        // creates regex pattern to include set of special characters
+        Pattern specialChar = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        // converts matcher to boolean via find()
+        return specialChar.matcher(pw).find();
+    }
+
+
+
+}
---
+++ src/test/java/PasswordCheckerTest.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordCheckerTest {
+    // hasMinLength
+    @Test
+    void hasMinLengthShouldReturnFalseWhenTooShort() {
+        assertFalse(PasswordChecker.hasMinLength("testpw"));
+    }
+
+    @Test
+    void hasMinLengthShouldReturnTrueWhen8() {
+        assertTrue(PasswordChecker.hasMinLength("testtest"));
+
+    }
+
+    @Test
+    void hasMinLengthShouldReturnTrueWhen12() {
+        assertTrue(PasswordChecker.hasMinLength("testtesttest"));
+    }
+
+    // containsDigits
+    @Test
+    void containsDigitsShouldReturnFalseWhenWithoutDigits() {
+        assertFalse(PasswordChecker.containsDigits("test"));
+
+    }
+
+    @Test
+    void containsDigitsShouldReturnTrueWhenWithDigits() {
+        assertTrue(PasswordChecker.containsDigits("test123"));
+
+    }
+
+    // hasUpperAndLower
+    @Test
+    void hasUpperAndLowerReturnsFalseWhenOnlyLower() {
+        assertFalse(PasswordChecker.hasUpperAndLower("test"));
+    }
+
+    @Test
+    void hasUpperAndLowerReturnsFalseWhenOnlyUpper() {
+        assertFalse(PasswordChecker.hasUpperAndLower("TEST"));
+    }
+
+    @Test
+    void hasUpperAndLowerReturnsTrueWhenUpperAndLower() {
+        assertTrue(PasswordChecker.hasUpperAndLower("TeSt"));
+    }
+
+    // isNotCommonPW
+    @Test
+    void isNotCommonPWShouldReturnFalseWhenPassword() {
+        assertFalse(PasswordChecker.isNotCommonPW("Password"));
+    }
+
+    @Test
+    void isNotCommonPWShouldReturnTrueWhenTest123() {
+        assertTrue(PasswordChecker.isNotCommonPW("Test123"));
+    }
+
+    // hasSpecialChars
+    @Test
+    void hasSpecialCharsShouldReturnFalseWhenNoSpecialChars() {
+        assertFalse(PasswordChecker.hasSpecialChars("test123TEST456"));
+    }
+
+    @Test
+    void hasSpecialCharsShouldReturnTrueWhenWithPercent() {
+        assertTrue(PasswordChecker.hasSpecialChars("bla%bla"));
+    }
+
+    @Test
+    void hasSpecialCharsShouldReturnTrueWhenWithQuestionMarks() {
+        assertTrue(PasswordChecker.hasSpecialChars("?bla?bla?"));
+    }
+}
